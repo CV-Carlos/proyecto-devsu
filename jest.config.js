e80dea5732bd6f4a989a24d5b80407d7610fda5b
ccerd <ccerd@DESKTOP-JMA1LVA>
2026-02-15 12:04:18 -0500

    initial configuration - folder structure and jest

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/dist/'
+  ],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!src/app/**/*.module.ts',
+    '!src/app/**/*.interface.ts',
+    '!src/app/**/*.model.ts',
+    '!src/app/main.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  },
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@environments/(.*)': '<rootDir>/src/environments/$1'
+  }
+};
